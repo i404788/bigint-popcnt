4aad6d8b48d053cbe6766b540b244aa705168b95
wholenews <wholenewsorg@gmail.com>
2020-06-19 12:45:44 -0700

    Don't use bigint literals to support Node10

--- bench.ts
+++ bench.ts
@@ -1,18 +1,19 @@
 import {forwardmask, backwardmask, fastpopcnt} from "./index";
 
 console.log('Popcnt benchmark')
+const one = BigInt(1)
 
 // O(bits)
 function popcnt(v: bigint) {
     let c = 0
     for (; v; c++) {
-        v &= v - 1n; // clear the least significant bit set
+        v &= v - one; // clear the least significant bit set
     }
     return c
 }
 
 const samples = 100000
-const sample = (1n << 2048n)-1n
+const sample = (one << BigInt(2048))-one
 let res = 0
 let start = Date.now()
 for (let i = 0; i < samples; i++) res = popcnt(sample)
@@ -20,7 +21,7 @@ let time = Date.now() - start
 console.log(`popcnt: ${res}, samples: ${samples}, ${time}ms, ${samples / time}p/ms`)
 
 start = Date.now()
-const max_mask = 8192n
+const max_mask = BigInt(8192)
 const _popcnt = fastpopcnt(max_mask)
 for (let i = 0; i < samples; i++) res = Number(_popcnt(sample))
 time = Date.now() - start
--- index.ts
+++ index.ts
@@ -1,28 +1,31 @@
+const zero = BigInt(0)
+const one = BigInt(1)
+
 // http://graphics.stanford.edu/~seander/bithacks.html
 export function popcnt(v: bigint) {
-    let c = 0n
+    let c = zero
     for (; v; c++) {
-        v &= v - 1n; // clear the least significant bit set
+        v &= v - one; // clear the least significant bit set
     }
     return c
 }
 
 export function forwardmask(bits: bigint) {
     let masks: bigint[] = []
-    for (let n = 1n; n < bits;){
+    for (let n = one; n < bits;){
         masks = masks.map(x => x | (x << n))
-        masks.push((1n << n) - 1n)
-        n <<= 1n;
+        masks.push((one << n) - one)
+        n <<= one;
     }
     return masks
 }
 
 export function backwardmask(bits: bigint) {
-    let mask = (1n << bits) - 1n
+    let mask = (one << bits) - one
     const fmask = mask
     let masks: bigint[] = []
     while (bits > 1) {
-        bits >>= 1n
+        bits >>= one
         mask = ((mask << bits) ^ mask) & fmask
         masks.push(mask)
     }
@@ -37,12 +40,12 @@ export function backwardmask(bits: bigint) {
 // See https://blog.devdroplets.com/generalized-hamming-weight/
 export function fastpopcnt(bits: bigint) {
     // Not worth the time generating masks
-    if (bits <= 128n) return popcnt
+    if (bits <= BigInt(128)) return popcnt
 
     // Convert to power of 2 (e.g.) 118 -> 128, 24 -> 32
     let approx_bits = bits
-    bits = 1n;
-    while(approx_bits > bits) bits <<= 1n;
+    bits = one;
+    while(approx_bits > bits) bits <<= one;
 
     // Initialize masks
     let masks = 
@@ -51,10 +54,10 @@ export function fastpopcnt(bits: bigint) {
 
     // Run constant-time popcnt
     return (v: bigint) => { 
-        let bits = 1n
+        let bits = one
         for (const mask of masks) {
             v = (v & mask) + ((v & ~mask) >> bits)
-            bits <<= 1n
+            bits <<= one
         }
         return v
     }
